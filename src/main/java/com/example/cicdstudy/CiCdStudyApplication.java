27898e514fc89aa4ac19f162b42fdab03d7201f2
박병욱 <quddnr2688@gmail.com>
2024-10-23 14:26:16 +0900

    init

---
+++ src/main/java/com/example/cicdstudy/CiCdStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.cicdstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CiCdStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cicdstudy/CiCdStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cicdstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
